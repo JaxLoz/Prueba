ec2758eb472d5ff0bf29e80fe81e7ff17595fab1
JavierM-Linux <jaxloz2002@gmail.com>
2021-09-13 15:08:57 -0500

    se agrego el codigo que permite intruduccir datos a la matriz desde la linea 29 hasta la 40 lo y se realizon un codigo provicional que muestra la informacion de la matriz

--- src/matrices/Matrices.java
+++ src/matrices/Matrices.java
@@ -23,8 +23,31 @@ public class Matrices {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int Nfilas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de filas "));;
+        int Nfilas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de filas"));;
         int Ncol = Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero de columnas"));
+        
+        sd matriz = new sd(Nfilas, Ncol);
+        
+        for (int i = 0; i < Nfilas; i++){
+            for (int j = 0; j < Ncol; j++){
+                
+                int Info = Integer.parseInt(JOptionPane.showInputDialog("ingrese el dato para la coordenada "+"("+(i+1)+","+(j+1)+")"+" de la matriz"
+                        +"\n"+"Tamaño de la matriz: "+Nfilas+"x"+Ncol));
+                
+                matriz.setMatriz(i, j, Info);
+            
+            }  
+        }
+        
+        System.out.println("Informacion de la Matriz:");
+        for (int i = 0; i < Nfilas; i++){
+            for (int j = 0; j < Ncol; j++){
+                
+                System.out.println(matriz.getMatriz(i, j));
+                
+            }
+        }
+        
         
     }
     
